bc1f897138f0db7ef12494cca8181667dcdc3e4d
Alexey Zarakovskiy <alexey.zarakovskiy@fashiontrade.com>
2019-09-30 14:48:54 +0200

    Wrong paths in git.zsh

--- git/git.zsh
+++ git/git.zsh
@@ -1,2 +1,2 @@
-source ${funcsourcetrace[1]%/*}/git/git_pr.zsh
-source ${funcsourcetrace[1]%/*}/git/git_rebase_onto_master.zsh
+source ${funcsourcetrace[1]%/*}/git_pr.zsh
+source ${funcsourcetrace[1]%/*}/git_rebase_onto_master.zsh
