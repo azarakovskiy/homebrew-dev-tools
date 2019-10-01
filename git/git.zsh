8f341abff2c71aa59c5ab165c8b7e6a3db474d30
Alexey Zarakovskiy <4928787+azarakovskiy@users.noreply.github.com>
2019-10-01 12:57:40 +0200

    updated git.zsh with all the commands

--- git/git.zsh
+++ git/git.zsh
@@ -1,2 +1,4 @@
 source ${funcsourcetrace[1]%/*}/git_pr.zsh
+source ${funcsourcetrace[1]%/*}/git_cleanup.zsh
 source ${funcsourcetrace[1]%/*}/git_rebase_onto_master.zsh
+source ${funcsourcetrace[1]%/*}/git_squash.zsh
