8cbe6e9054ea0d706eadbcbf8a0cd83debc9c3e1
Alexey Zarakovskiy <alexey.zarakovskiy@fashiontrade.com>
2019-10-04 13:48:31 +0200

    git checkout branch / gitbr

--- git/git.zsh
+++ git/git.zsh
@@ -1,4 +1,5 @@
 source ${funcsourcetrace[1]%/*}/git_pr.zsh
 source ${funcsourcetrace[1]%/*}/git_cleanup.zsh
 source ${funcsourcetrace[1]%/*}/git_rebase_onto_master.zsh
+source ${funcsourcetrace[1]%/*}/git_new_branch.zsh
 source ${funcsourcetrace[1]%/*}/git_squash.zsh
---
+++ git/git_new_branch.zsh
@@ -0,0 +1,19 @@
+function git_branch_checkout() {
+    BRANCH_NAME=$1
+    git rev-parse --verify ${BRANCH_NAME} &> /dev/null
+
+    if [[ $? == 0 ]]; then
+        git checkout ${BRANCH_NAME}
+        if [[ $? == 0 ]]; then
+            echo "Checked out existing branch"
+        fi
+    else 
+        git checkout -b ${BRANCH_NAME}
+        if [[ $? == 0 ]]; then
+            echo "Created and checked out a new branch"
+        fi
+    fi
+}
+
+# Checkout a branch or create a new one
+alias gitbr=git_branch_checkout
