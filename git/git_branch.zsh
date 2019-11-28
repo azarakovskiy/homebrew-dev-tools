0bd1f4490a431e3984b842c768539250543c2ca2
Alexey Zarakovskiy <dev@azarakovskiy.com>
2019-11-28 11:26:27 +0100

    Added `git checkout -` option to `gbranch`

--- git/git_branch.zsh
+++ git/git_branch.zsh
@@ -1,4 +1,4 @@
-function git_branch_checkout() {
+function git_branch_verify_and_checkout() {
     BRANCH_NAME=$1
     git rev-parse --verify ${BRANCH_NAME} &> /dev/null
 
@@ -13,6 +13,17 @@ function git_branch_checkout() {
             echo "Created and checked out a new branch"
         fi
     fi
+}
+
+function git_branch_checkout() {
+    BRANCH_NAME=$1
+
+    if [[ $BRANCH_NAME == "" ]] || [[ $BRANCH_NAME == "-" ]]; then
+        git checkout -
+        echo "Checked out previous branch (git checkout -)"
+    else        
+        git_branch_verify_and_checkout $BRANCH_NAME
+    fi    
 }
 
 # Checkout a branch or create a new one
