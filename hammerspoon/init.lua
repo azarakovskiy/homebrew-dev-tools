2d905f2ba5902de5c060db56b6a038eeec296142
Alexey Zarakovskiy <dev@azarakovskiy.com>
2020-02-13 18:48:08 +0100

    Added the config for Hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,35 @@
+majorMash = {"cmd", "alt", "ctrl"}
+minorMash = {"shift", "alt", "ctrl"}
+
+hs.loadSpoon("SpoonInstall", {
+    use_syncinstall = true
+})
+
+-- todo add multiple repos
+
+-- spoon.SpoonInstall.repos.HammerspoonShiftit = {
+--     url = "https://github.com/azarakovskiy/hammerspoon-shiftit",
+--     desc = "HammerspoonShiftit spoon repository",
+--     branch = "master",
+-- }
+-- spoon.SpoonInstall:updateRepo('HammerspoonShiftit')
+Install=spoon.SpoonInstall
+
+
+Install:andUse("Caffeine", {
+    start = true, hotkeys = { 
+        toggle = { minorMash, "c" }
+    }
+})
+
+Install:andUse("MouseCircle", {    
+    config = {
+        color = hs.drawing.color.x11.darkred        
+    },
+    hotkeys = { 
+        show = { minorMash, "m" }
+    }
+})
+
+hs.loadSpoon("HammerspoonShiftit")
+spoon.HammerspoonShiftit:bindHotkeys({});
