8462c63235e5fae98025f9d182cdb5015476f737
Alexey Zarakovskiy <4928787+azarakovskiy@users.noreply.github.com>
2021-01-07 11:46:08 +0100

    Add docker tools to all tools

--- all.zsh
+++ all.zsh
@@ -1,3 +1,4 @@
 source ${funcsourcetrace[1]%/*}/git/git.zsh
 source ${funcsourcetrace[1]%/*}/cli/cli.zsh
+source ${funcsourcetrace[1]%/*}/cli/docker.zsh
 source ${funcsourcetrace[1]%/*}/kube/kube.zsh
