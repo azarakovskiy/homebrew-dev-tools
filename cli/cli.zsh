a7a82a7b25bfe13c83a7f4026d451cff8a80b311
Alexey Zarakovskiy <4928787+azarakovskiy@users.noreply.github.com>
2021-10-13 11:09:36 +0200

    Remove sublime magic

--- cli/cli.zsh
+++ cli/cli.zsh
@@ -5,22 +5,6 @@ bindkey "^[[1;9C" forward-word
 # Mouse speed
 defaults write -g com.apple.mouse.scaling 9.0
 
-# Sublime
-SUBL_BIN="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
-SUBLIME=/usr/local/bin/sublime
-SUBL=/usr/local/bin/subl
-if [ -f $SUBL_BIN ]; then
-	if [ ! -L $SUBLIME ]; then
-		ln -sv $SUBL_BIN $SUBLIME
-	fi
-	if [ ! -L $SUBL ]; then
-		ln -sv $SUBL_BIN $SUBL
-	fi
-else
-	echo "## Please install Sublime editor. It's good."
-fi
-
-
 # Quick access aliases to .zshrc
 alias zxc='sublime ~/.zshrc'
 alias zxcv='source ~/.zshrc'
