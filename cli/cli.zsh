c57e540831c9182b0966e236ddf09302ed30f9bf
Alexey Zarakovskiy <dev@azarakovskiy.com>
2021-10-15 17:01:13 +0200

    Fix the cli issues

--- cli/cli.zsh
+++ cli/cli.zsh
@@ -30,23 +30,23 @@ alias grepl=grepl_impl
 # use tool version
 func use() {
     declare -A _roots=(
-        ["go 1.13"]="/usr/local/Cellar/go@1.13/1.13.15/libexec"
-        ["go 1.14"]="/usr/local/Cellar/go@1.14/1.14.15/libexec" 
-	["go 1.15"]="/usr/local/Cellar/go@1.15/1.15.9/libexec" 
-	["go 1.16"]="/usr/local/Cellar/go@1.16/1.16.9/libexec" 
-	["go 1.16"]="/usr/local/Cellar/go/1.17/libexec" 
-	["go latest"]="/usr/local/Cellar/go/1.17/libexec" 
+      ["go 1.13"]="/usr/local/Cellar/go@1.13/1.13.15/libexec"
+      ["go 1.14"]="/usr/local/Cellar/go@1.14/1.14.15/libexec"
+    	["go 1.15"]="/usr/local/Cellar/go@1.15/1.15.9/libexec"
+    	["go 1.16"]="/usr/local/Cellar/go@1.16/1.16.9/libexec"
+    	["go 1.17"]="/usr/local/Cellar/go/1.17/libexec"
+    	["go latest"]="/usr/local/Cellar/go/1.17/libexec"
     )
     declare -A _versions=(
         ["go 1.13"]="/usr/local/Cellar/go@1.13/1.13.15"
-        ["go 1.14"]="/usr/local/Cellar/go@1.14/1.14.15" 
-	["go 1.15"]="/usr/local/Cellar/go@1.15/1.15.9" 
-	["go 1.16"]="/usr/local/Cellar/go@1.16/1.16.9" 
-	["go 1.17"]="/usr/local/Cellar/go/1.17/libexec" 
-	["go latest"]="/usr/local/Cellar/go/1.17/libexec" 
+        ["go 1.14"]="/usr/local/Cellar/go@1.14/1.14.15"
+      	["go 1.15"]="/usr/local/Cellar/go@1.15/1.15.9"
+      	["go 1.16"]="/usr/local/Cellar/go@1.16/1.16.9"
+      	["go 1.17"]="/usr/local/Cellar/go/1.17/libexec"
+      	["go latest"]="/usr/local/Cellar/go/1.17/libexec"
     )
     declare -A _tools=(
-        ["go"]=_setGoVersion 
+        ["go"]=_setGoVersion
     )
 
     readonly name=${1:?"Specify what you want to use"}
@@ -54,11 +54,11 @@ func use() {
 
     p1="$name"
     p2="$name $version"
-    
+
     fn=$_tools[$p1]
     v=$_versions[$p2]
     r=$_versions[$p2]
-    
+
     "$fn" "$v" "$r"
 }
 
@@ -66,8 +66,8 @@ func _setGoVersion() {
     readonly version=$1
     readonly goroot=$2
     echo $version
-    ln -sf "$version/bin/go" /usr/local/bin/go 
-    ln -sf "$version/bin/godoc" /usr/local/bin/godoc 
+    ln -sf "$version/bin/go" /usr/local/bin/go
+    ln -sf "$version/bin/godoc" /usr/local/bin/godoc
     ln -sf "$version/bin/gofmt" /usr/local/bin/gofmt
     export GOROOT=$goroot
 }
