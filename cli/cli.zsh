d17fd1e5fba9fa8071a906361d0083a81af1894e
Alexey Zarakovskiy <dev@azarakovskiy.com>
2022-04-14 13:46:57 +0200

    Add go@1.17

--- cli/cli.zsh
+++ cli/cli.zsh
@@ -34,16 +34,18 @@ func use() {
       ["go 1.14"]="/usr/local/Cellar/go@1.14/1.14.15/libexec"
     	["go 1.15"]="/usr/local/Cellar/go@1.15/1.15.9/libexec"
     	["go 1.16"]="/usr/local/Cellar/go@1.16/1.16.9/libexec"
-    	["go 1.17"]="/usr/local/Cellar/go/1.17/libexec"
-    	["go latest"]="/usr/local/Cellar/go/1.17/libexec"
+    	["go 1.17"]="/usr/local/Cellar/go@1.17/1.17.9/libexec"
+      ["go 1.18"]="/usr/local/Cellar/go/1.18/libexec"
+    	["go latest"]="/usr/local/Cellar/go/1.18/libexec"
     )
     declare -A _versions=(
         ["go 1.13"]="/usr/local/Cellar/go@1.13/1.13.15"
         ["go 1.14"]="/usr/local/Cellar/go@1.14/1.14.15"
       	["go 1.15"]="/usr/local/Cellar/go@1.15/1.15.9"
       	["go 1.16"]="/usr/local/Cellar/go@1.16/1.16.9"
-      	["go 1.17"]="/usr/local/Cellar/go/1.17/libexec"
-      	["go latest"]="/usr/local/Cellar/go/1.17/libexec"
+      	["go 1.17"]="/usr/local/Cellar/go@1.17/1.17.9"
+        ["go 1.18"]="/usr/local/Cellar/go/1.18"
+      	["go latest"]="/usr/local/Cellar/go/1.18"
     )
     declare -A _tools=(
         ["go"]=_setGoVersion
