a2597f0584459edcea97dd008409fa4f5947e4c4
Alexey Zarakovskiy <alexey.zarakovskiy@personio.de>
2024-05-22 11:49:03 +0200

    add a function to prune containers volumes

--- cli/docker.zsh
+++ cli/docker.zsh
@@ -2,4 +2,35 @@ function docker_stop_all_containers() {
   docker ps -q | xargs docker stop
 }
 
+function docker_prune_volumes() {
+    local container_id="$1"
+
+    # Check if container ID is provided
+    if [ -z "$container_id" ]; then
+        echo "Container ID is missing."
+        return 1
+    fi
+
+    # Check if container exists
+    if ! docker inspect "$container_id" >/dev/null 2>&1; then
+        echo "Container $container_id does not exist."
+        return 1
+    fi
+
+    # List associated volumes
+    volumes=$(docker inspect --format '{{ range .Mounts }}{{ if .Name }}{{ .Name }} {{ end }}{{ end }}' "$container_id")
+
+    docker stop "$container_id"
+    docker rm "$container_id"
+    echo "Container $container_id has been removed."
+
+    # Remove associated volumes
+    for volume in $volumes; do
+        docker volume rm "$volume"
+    done
+
+    echo "Volumes associated with container $container_id have been removed."
+}
+
 alias dockstop=docker_stop_all_containers
+alias dockrmvol=docker_prune_volumes
